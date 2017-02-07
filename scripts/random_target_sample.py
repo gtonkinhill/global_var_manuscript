729dcc31595e0e613f5498f99707b15ea5c5ad70
Gerry Tonkin-Hill <tonkin-hill.g@wehi.edu.au>
2017-02-07 18:13:22 +0000

    added script to get random sample for jump estimation

---
+++ scripts/random_target_sample.py
@@ -0,0 +1,30 @@
+from mungo.fasta import FastaReader
+import sys, os
+import random
+
+SAMPLE_SIZE=1000
+seqs = {}
+seq_list = []
+count=0
+input=sys.argv[1]
+
+with open(input + "_mapping.txt", 'w') as outfile:
+  for h,s in FastaReader(input):
+    count+=1
+    outfile.write("seq" + str(count) + "\t" + h + "\n")
+    seqs["seq" + str(count)] = s
+    seq_list.append("seq" + str(count))
+
+rand_seq_list = seq_list[:]
+random.shuffle(rand_seq_list)
+
+assert seq_list[0]!=rand_seq_list[0], "Lists match!"
+
+with open(input[:-6] + "_randomSampleTargetSize" + str(SAMPLE_SIZE) +".fasta", 'w') as outfile:
+  count=0
+  for s in rand_seq_list:
+    if count<SAMPLE_SIZE:
+      outfile.write(">"+"target_"+t+"\n"+seqs[s]+"\n")
+    else:
+      outfile.write(">"+"db_"+s+"\n"+seqs[s]+"\n")
+    count+=1
